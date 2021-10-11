6bfcb9866e7a8ce38378fb5a19be0e741d867c93
ameerhamza01 <ameerangular@codesorbit.com>
2021-10-11 18:35:15 +0500

    server port changes

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/*', (req, res) =>
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
   console.log('server is running on port 8080')
 });
